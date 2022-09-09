18ddbb858185d4fbd15fec44aaf3c0b94e9ee44c
k1tra <jepp880d@stud.kea.dk>
2022-09-09 13:52:25 +0100

    Tilføjet metoden greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
     public void start() {
         System.out.println("Det virker!");
     }
+    Scanner sc = new Scanner(System.in);
+    public String greet () {
+        System.out.println("Indtast dit navn");
+       String navn = sc.next();
+        return navn;
+    }
     public static void main(String[] args){
         SuperGreeter sg = new SuperGreeter();
         sg.start();
+        sg.greet();
     }
+
+
 }
